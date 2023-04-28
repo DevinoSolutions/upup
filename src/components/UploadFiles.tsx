9f197794b11eeb2493c0b9789672ddc7d49b673f
BASSEM <bassem.jadoui@esprit.tn>
2023-04-28 16:54:26 +0100

    handle s3 upload fail access-origin

--- src/components/UploadFiles.tsx
+++ src/components/UploadFiles.tsx
@@ -1,11 +1,13 @@
 import React, {useState, DragEvent, FC} from 'react';
 import FileUploader from './FileUploader/FileUploader';
+import pako from 'pako'
 
 export interface UploadFilesProps {
-    children?: React.ReactNode;
+    client: any
+    bucket: string
 }
 
-export const  UploadFiles: FC<UploadFilesProps> = () =>  {
+export const UploadFiles: FC<UploadFilesProps>  = ({client,bucket}: UploadFilesProps) => {
     const [dragging, setDragging] = useState<boolean>(false)
     const handleDragEnter = (e: DragEvent<HTMLDivElement>) => {
         e.preventDefault()
@@ -13,8 +15,40 @@ export const  UploadFiles: FC<UploadFilesProps> = () =>  {
         setDragging(() => true)
     }
     const [files, setFiles] = useState<File[]>([]);
+    const handleUpload = async () => {
+        const formData = new FormData();
+        if (files) {
+            for (let i = 0; i < files.length; i++) {
+                const element = files[i];
+                const buffer: ArrayBuffer = await element.arrayBuffer();
+                const compressedFile = new File(
+                    [pako.gzip(buffer)],
+                    element.name + ".gz",
+                    {
+                        type: "application/octet-stream"
+                    }
+                );
+                formData.append("file", compressedFile);
+            }
+        }
 
+        let key = '';
 
+        files.forEach(element => {
+            key = `${Date.now()}__${element.name}`
+            client.putObject(
+                {
+                    Bucket: bucket,
+                    Key: `${key}`,
+                    Body: element,
+                    ACL: 'public-read',
+                },
+                (err:any, _data:any) => {
+                    if (err) console.log(err, err.stack)
+                }
+            )
+        })
+    }
     return (
         <div
             className="max-w-full overflow-hidden flex w-full"
@@ -27,6 +61,13 @@ export const  UploadFiles: FC<UploadFilesProps> = () =>  {
                 setFiles={setFiles}
                 multiple={false}
             />
+            <button
+                className="bg-primary text-white px-4 py-2 rounded-md"
+                onClick={handleUpload}
+
+            >
+                Upload
+            </button>
 
         </div>
     )
