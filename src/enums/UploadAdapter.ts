c6e826f70e14208d0cb821f267b6121c28db6b21
BASSEM <bassem.jadoui@esprit.tn>
2023-06-03 23:33:28 +0100

    upload adapter separation
    Took 1 hour 28 minutes

--- src/enums/UploadAdapter.ts
+++ src/enums/UploadAdapter.ts
@@ -1,11 +1,3 @@
-/**
- * UploadAdapter enum to control which upload provider the user want to use to upload the file
- * @enum
- * @type {{internal: number, google_drive: number, one_drive: number}}
- * @property {number} internal - internal storage
- * @property {number} google_drive - google drive storage provider (https://developers.google.com/drive/api/v3/about-sdk)
- * @property {number} one_drive - one drive storage provider (https://docs.microsoft.com/en-us/onedrive/developer/rest-api/api/driveitem_put_content?view=odsp-graph-online)
- */
 export enum UploadAdapter {
   internal,
   google_drive,
