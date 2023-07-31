3e151247d77f3afdfb5aa10a97430446a818b0e5
BASSEM <bassem.jadoui@esprit.tn>
2023-07-31 17:32:11 +0100

    refactor storybook/main.ts
    Took 4 minutes

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,7 +5,7 @@ import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
   framework: '@storybook/react-webpack5',
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../**/*.mdx', '../**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     // Other addons go here
     {
