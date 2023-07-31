b5f2f3d19b62fc4de4235bb5e297bb4dceac3722
BASSEM <bassem.jadoui@esprit.tn>
2023-07-31 18:18:47 +0100

    showing upupUploader components in storybook done
    Took 27 minutes

--- stories/UploadFiles.stories.tsx
+++ stories/UploadFiles.stories.tsx
@@ -1,16 +1,13 @@
 import { Meta, Story } from '@storybook/react';
 import { UploadFiles, UploadFilesProps } from '../src/components/UploadFiles';
+import { IBaseConfigs, UploadAdapter, UpupUploader } from '../src';
+import useUpup from '../src/hooks/useUpup';
+import { UpupUploaderProps } from '../src/UpupUploader';
 
 const meta: Meta = {
   title: 'Upload files',
   component: UploadFiles,
-  argTypes: {
-    children: {
-      control: {
-        type: 'text',
-      },
-    },
-  },
+  argTypes: {},
   parameters: {
     controls: { expanded: true },
   },
@@ -18,7 +15,25 @@ const meta: Meta = {
 
 export default meta;
 
-const Template: Story<UploadFilesProps> = (args) => <UploadFiles {...args} />;
+const Template: Story<UpupUploaderProps> = (args) => {
+  const { baseConfigs, cloudStorageConfigs, googleConfigs, oneDriveConfigs } =
+    useUpup();
+  const uploadAdapters: UploadAdapter[] = [
+    UploadAdapter.internal,
+    UploadAdapter.google_drive,
+    UploadAdapter.one_drive,
+  ];
+  return (
+    <UpupUploader
+      {...args}
+      baseConfigs={baseConfigs}
+      uploadAdapters={uploadAdapters}
+      cloudStorageConfigs={cloudStorageConfigs}
+      googleConfigs={googleConfigs}
+      oneDriveConfigs={oneDriveConfigs}
+    />
+  );
+};
 
 // By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
 // https://storybook.js.org/docs/react/workflows/unit-testing
