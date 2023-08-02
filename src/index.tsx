3ad3fc202455bff04d5b32ef2e79d764ce237a41
BASSEM <bassem.jadoui@esprit.tn>
2023-08-02 17:53:56 +0100

    export UploadAdapter type
    Took 1 minute

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 // Author: Bassem JADOUI 31/05/2023
 
 export { UpupUploader } from './UpupUploader'
-export { UploadAdapter } from './types/UploadAdapter'
+export { UploadAdapter, UPLOAD_ADAPTER } from './types/UploadAdapter'
 export { S3Configs } from './types/S3Configs'
 export { BaseConfigs } from './types/BaseConfigs'
 export { CloudStorageConfigs } from './types/CloudStorageConfigs'
