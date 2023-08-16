42485bf24617affde6a119b27b97cf541497b33f
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-08-16 11:04:20 +0100

    Preview files

--- src/components/UpupUploader/Preview.tsx
+++ src/components/UpupUploader/Preview.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { AnimatePresence, motion } from 'framer-motion'
+import { TbX } from 'react-icons/tb'
 
 const Preview = ({
     files,
@@ -7,33 +8,84 @@ const Preview = ({
 }: {
     files: File[]
     setFiles: (files: File[]) => void
-}) => (
-    <AnimatePresence>
-        {files.length > 0 ? (
-            <motion.div
-                key={'files-preview'}
-                initial={{ scaleY: '0%', height: '3rem' }}
-                animate={{ scaleY: '100%' }}
-                exit={{ scaleY: '0%', height: '0rem' }}
-                className="h-12 bg-[#fafafa] border-b flex justify-between items-center p-2 text-sm text-[#1b5dab] font-medium origin-top"
-            >
-                <button
-                    className="hover:bg-[#e9ecef] active:bg-[#dfe6f1] rounded-md p-2 px-4 transition-all duration-300"
-                    onClick={() => setFiles([])}
-                >
-                    Cancel
-                </button>
-                <p className="text-[#333]">
-                    {files.length} file{files.length > 1 ? 's' : ''} selected
-                </p>
-                <button className="hover:bg-[#e9ecef] active:bg-[#dfe6f1] rounded-md p-2 px-4 transition-all duration-300">
-                    Add more
-                </button>
-            </motion.div>
-        ) : (
-            <div></div>
-        )}
-    </AnimatePresence>
-)
+}) => {
+    const removeFile = (index: number) => {
+        const newFiles = files.filter((_, i) => i !== index)
+        setFiles(newFiles)
+    }
+
+    return (
+        <AnimatePresence>
+            {files.length > 0 && (
+                <motion.div className="h-full w-full grid grid-rows-[auto,1fr,auto] text-sm font-medium absolute z-10">
+                    <motion.div
+                        initial={{ scaleY: '0%' }}
+                        animate={{ scaleY: '100%' }}
+                        exit={{ scaleY: '0%' }}
+                        className="flex justify-between items-center p-2 border-b bg-[#fafafa] text-[#1b5dab] origin-top"
+                    >
+                        <button
+                            className="hover:bg-[#e9ecef] active:bg-[#dfe6f1] rounded-md p-2 px-4 transition-all duration-300"
+                            onClick={() => setFiles([])}
+                        >
+                            Cancel
+                        </button>
+                        <p className="text-[#333]">
+                            {files.length} file{files.length > 1 ? 's' : ''}{' '}
+                            selected
+                        </p>
+                        <button className="hover:bg-[#e9ecef] active:bg-[#dfe6f1] rounded-md p-2 px-4 transition-all duration-300">
+                            Add more
+                        </button>
+                    </motion.div>
+
+                    <motion.div
+                        initial={{ scaleY: '0%' }}
+                        animate={{ scaleY: '100%' }}
+                        exit={{ scaleY: '0%' }}
+                        className="grid grid-cols-4 p-4 border-b bg-[#f4f4f4] origin-bottom gap-4 overflow-y-scroll"
+                    >
+                        {files.map((file, i) => (
+                            <div
+                                key={i}
+                                className="flex flex-col items-start h-full w-full gap-1 relative"
+                            >
+                                <img
+                                    src={URL.createObjectURL(file)}
+                                    alt=""
+                                    className="h-40 w-full rounded-md object-cover shadow"
+                                />
+                                <p className="font-medium text-xs">
+                                    {file.name}
+                                </p>
+                                <button
+                                    className="bg-black rounded-full absolute -top-1 -right-1"
+                                    onClick={() => removeFile(i)}
+                                >
+                                    <TbX className="h-4 w-4 text-white" />
+                                </button>
+                            </div>
+                        ))}
+                    </motion.div>
+
+                    <motion.div
+                        initial={{ scaleY: '0%' }}
+                        animate={{ scaleY: '100%' }}
+                        exit={{ scaleY: '0%' }}
+                        className="flex justify-start items-center p-3 border-b bg-[#fafafa] text-white origin-bottom"
+                    >
+                        <button
+                            className="bg-green-500 hover:bg-green-600 active:bg-green-700 rounded-md p-3 px-6 transition-all duration-300"
+                            onClick={() => setFiles([])}
+                        >
+                            Upload {files.length} file
+                            {files.length > 1 ? 's' : ''}
+                        </button>
+                    </motion.div>
+                </motion.div>
+            )}
+        </AnimatePresence>
+    )
+}
 
 export default Preview
