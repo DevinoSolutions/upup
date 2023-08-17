da0e4a4f66d9ed6624aecb5c450e2c1405f8c110
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-08-17 08:11:53 +0100

    fix text color on hover while in dark mode

--- src/components/UpupUploader/View.tsx
+++ src/components/UpupUploader/View.tsx
@@ -21,9 +21,9 @@ const View = ({
                     exit={{ y: '-100%' }}
                     className="absolute h-full w-full grid grid-rows-[auto,1fr] z-20 "
                 >
-                    <div className="h-12 bg-[#fafafa] border-b flex justify-between items-center p-2 text-sm text-[#1b5dab] font-medium dark:bg-[#1f1f1f] dark:text-[#fafafa]">
+                    <div className="h-12 bg-[#fafafa] border-b flex justify-between items-center p-2 text-sm text-[#1b5dab] font-medium dark:bg-[#1f1f1f] dark:text-[#fafafa] group">
                         <button
-                            className="hover:bg-[#e9ecef] active:bg-[#dfe6f1] rounded-md p-2 px-4 transition-all duration-300"
+                            className="hover:bg-[#e9ecef] active:bg-[#dfe6f1] rounded-md p-2 px-4 transition-all duration-300 dark:group-hover:text-[#1f1f1f]"
                             onClick={() => setView('internal')}
                         >
                             Cancel
