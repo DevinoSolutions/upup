4e0cba1f052797e6decb6f54d4439f705fe90201
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-08-18 09:51:37 +0100

    receive dark mode from parent

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ['./**/*.{tsx,ts,css}'],
+    darkMode: 'class', // or 'media' or 'class'
     theme: {
         extend: {},
     },
