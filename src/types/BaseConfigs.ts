d7cb317292d6ccb1ba9e08eb2b0eac4cc2261b17
BASSEM <bassem.jadoui@esprit.tn>
2023-08-22 14:31:48 +0100

    remove canUpload and setKeys
    Took 5 minutes

--- src/types/BaseConfigs.ts
+++ src/types/BaseConfigs.ts
@@ -1,6 +1,4 @@
 export type BaseConfigs = {
-    setKeys: (keys: string[]) => void
-    canUpload: boolean
     toBeCompressed?: boolean
     multiple?: boolean
     onChange?: (files: File[]) => void
