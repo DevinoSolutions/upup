a54947a113a6404318e391aad9a5f7fb13949f9e
BASSEM <bassem.jadoui@esprit.tn>
2023-08-22 14:20:32 +0100

    enhance upload files logic
    Took 1 hour 15 minutes

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 // Author: Bassem JADOUI 31/05/2023
 import './tailwind.css'
 
-export { UpupUploader } from './UpupUploader'
+export { UpupUploader, UploadFilesRef } from './UpupUploader'
 export { UploadAdapter, UPLOAD_ADAPTER } from './types/UploadAdapter'
 export { S3Configs } from './types/S3Configs'
 export { BaseConfigs } from './types/BaseConfigs'
