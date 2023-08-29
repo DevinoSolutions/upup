1aa92295b5833b872cb833d351d10ecc96aef170
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-08-29 17:23:52 +0100

    update uplooad adapter type

--- src/types/UploadAdapter.ts
+++ src/types/UploadAdapter.ts
@@ -2,8 +2,15 @@ export const UploadAdapter = {
     INTERNAL: 'INTERNAL',
     GOOGLE_DRIVE: 'GOOGLE_DRIVE',
     ONE_DRIVE: 'ONE_DRIVE',
+    BOX: 'BOX',
     LINK: 'LINK',
     CAMERA: 'CAMERA',
+    DROPBOX: 'DROPBOX',
+    UNSPLASH: 'UNSPLASH',
+    FACEBOOK: 'FACEBOOK',
+    INSTAGRAM: 'INSTAGRAM',
+    AUDIO: 'AUDIO',
+    SCREENCAST: 'SCREENCAST',
 } as const
 
 type ObjectValues<T> = T[keyof T]
