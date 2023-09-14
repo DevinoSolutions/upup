e29dee2a06e353faec820c17ffc567ced2fe5899
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-09-14 11:57:22 +0100

    add tsup and bun

---
+++ tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig, Options } from 'tsup'
+
+export default defineConfig((options: Options) => ({
+    treeshake: true,
+    splitting: true,
+    entry: ['src/**/*.tsx'],
+    format: ['cjs'],
+    dts: true,
+    minify: true,
+    clean: true,
+    external: ['react', 'react-dom', 'react/jsx-runtime'],
+    ...options,
+}))
