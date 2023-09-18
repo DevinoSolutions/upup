de117e7972a00d9afc9c96bd63cfa63f50c6d90e
BASSEM <bassem.jadoui@esprit.tn>
2023-09-18 17:20:45 +0100

    await upload multiple document done !
    Took 1 hour 10 minutes

--- src/lib/uploadObject.ts
+++ src/lib/uploadObject.ts
@@ -1,10 +1,18 @@
-import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
+import { PutObjectCommand } from '@aws-sdk/client-s3'
+import { PutObjectRequest } from '@aws-sdk/client-s3/dist-types/models/models_0'
+import { S3ClientWithSend } from './getClient'
 
-interface props {
-    client: S3Client
+type props = {
+    client: S3ClientWithSend
     bucket: string
     key: string
     file: File
+}
+
+type UploadObjectResponse = {
+    httpStatusCode: number
+    message: string
+    key?: string
 }
 
 /**
@@ -14,37 +22,46 @@ interface props {
  * @param key the final file name, usually it has timestamp prefix
  * @param file file to upload
  */
-const uploadObject = async ({ client, bucket, key, file }: props) => {
+const uploadObject = async ({
+    client,
+    bucket,
+    key,
+    file,
+}: props): Promise<UploadObjectResponse> => {
     /**
      *   Define the parameters for the object you want to upload.
      */
-    const params = {
+    const params: PutObjectRequest = {
         Bucket: bucket, // The path to the directory you want to upload the object to, starting with your Space name.
         Key: `${key}`, // Object key, referenced whenever you want to access this file later.
         Body: file, // The object's contents. This variable is an object, not a string.
         ACL: 'public-read', // Defines ACL permissions, such as private or public.
     }
 
-    // Step 4: Define a function that uploads your object using SDK's PutObjectCommand object and catches any errors.
+    /**
+     *  Define a function that uploads your object using SDK's PutObjectCommand object and catches any errors.
+     */
     try {
-        console.log('Uploading object')
         const data = await client.send(new PutObjectCommand(params))
-        return data
-        // console.log('http', data)
-        // if (data.httpStatusCode == 200) {
-        //     console.log(
-        //         'Successfully uploaded object: ' +
-        //             params.Bucket +
-        //             '/' +
-        //             params.Key,
-        //     )
-        //     return params.Key
-        // }
+        if (data.$metadata.httpStatusCode === 200) {
+            return {
+                httpStatusCode: data.$metadata.httpStatusCode,
+                message:
+                    'Successfully uploaded object: ' +
+                    params.Bucket +
+                    '/' +
+                    params.Key,
+                key,
+            }
+        }
     } catch (err) {
-        console.log('Error', err)
+        console.error('Error', err)
     }
 
-    return null
+    return {
+        httpStatusCode: 500,
+        message: 'Error uploading object: ' + params.Bucket + '/' + params.Key,
+    }
 }
 
 // await client.putObject(
--- stories/Uploader.stories.tsx
+++ stories/Uploader.stories.tsx
@@ -1,58 +1,60 @@
-import { Meta } from "@storybook/react";
+import { Meta } from '@storybook/react'
 import {
-  UPLOAD_ADAPTER,
-  UploadAdapter,
-  UploadFilesRef,
-  UpupUploader
-} from "../src";
-import useUpup from "../src/hooks/useUpup";
-import { useRef } from "react";
+    UPLOAD_ADAPTER,
+    UploadAdapter,
+    UploadFilesRef,
+    UpupUploader,
+} from '../src'
+import useUpup from '../src/hooks/useUpup'
+import { useRef } from 'react'
 
 const meta: Meta<typeof UpupUploader> = {
-  title: "Uploader",
-  component: UpupUploader
-};
+    title: 'Uploader',
+    component: UpupUploader,
+}
 
-export default meta;
+export default meta
 
 const Uploader = args => {
-  const { baseConfigs, cloudStorageConfigs, googleConfigs, oneDriveConfigs } =
-    useUpup({
-      accept: "*",
-      multiple: true,
-      limit: 5,
-      onFileClick: file => void 0,
-      onFilesChange: files => void 0,
-      ...args
-    });
-  const uploadAdapters: UPLOAD_ADAPTER[] = [
-    UploadAdapter.INTERNAL,
-    UploadAdapter.GOOGLE_DRIVE,
-    UploadAdapter.ONE_DRIVE,
-    UploadAdapter.LINK,
-    UploadAdapter.CAMERA
-  ];
+    const { baseConfigs, cloudStorageConfigs, googleConfigs, oneDriveConfigs } =
+        useUpup({
+            accept: '*',
+            multiple: true,
+            limit: 5,
+            onFileClick: file => void 0,
+            onFilesChange: files => void 0,
+            ...args,
+        })
+    const uploadAdapters: UPLOAD_ADAPTER[] = [
+        UploadAdapter.INTERNAL,
+        UploadAdapter.GOOGLE_DRIVE,
+        UploadAdapter.ONE_DRIVE,
+        UploadAdapter.LINK,
+        UploadAdapter.CAMERA,
+    ]
 
-  const upupRef = useRef<UploadFilesRef>();
+    const upupRef = useRef<UploadFilesRef>()
 
-  const handleUpload = async () => {
-    await upupRef.current?.uploadFiles().then(data => console.log(data));
-  };
-  return (
-    <>
-      <UpupUploader
-        {...args}
-        baseConfigs={baseConfigs}
-        uploadAdapters={uploadAdapters}
-        cloudStorageConfigs={cloudStorageConfigs}
-        googleConfigs={googleConfigs}
-        oneDriveConfigs={oneDriveConfigs}
-        ref={upupRef}
-      />
-      <button onClick={handleUpload}>Upload</button>
-    </>
-  );
-};
+    const handleUpload = async () => {
+        await upupRef.current
+            ?.uploadFiles()
+            .then(data => console.log('data', data))
+    }
+    return (
+        <>
+            <UpupUploader
+                {...args}
+                baseConfigs={baseConfigs}
+                uploadAdapters={uploadAdapters}
+                cloudStorageConfigs={cloudStorageConfigs}
+                googleConfigs={googleConfigs}
+                oneDriveConfigs={oneDriveConfigs}
+                ref={upupRef}
+            />
+            <button onClick={handleUpload}>Upload</button>
+        </>
+    )
+}
 
-export const Default = () => Uploader({});
-export const Mini = () => Uploader({ mini: true });
+export const Default = () => Uploader({})
+export const Mini = () => Uploader({ mini: true })
