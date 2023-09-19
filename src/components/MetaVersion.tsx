c121a71564673d7f471e0b8f655011ced2069d08
BASSEM <bassem.jadoui@esprit.tn>
2023-09-19 11:23:39 +0100

    add meta version
    Took 53 minutes

---
+++ src/components/MetaVersion.tsx
@@ -0,0 +1,14 @@
+import * as packageJson from '~/package.json'
+
+const MetaVersion = () => {
+    return (
+        <>
+            <p className="text-xs text-[#9d9d9d] mb-4">
+                ًّ Upup v{packageJson.version}
+            </p>
+            <p className="text-xs text-[#9d9d9d] mb-4">Powered by uNotes</p>
+        </>
+    )
+}
+
+export default MetaVersion
