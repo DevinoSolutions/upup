84d08cfcf8f4374109d82d4f00e52e31cf2354cf
BASSEM <bassem.jadoui@esprit.tn>
2023-09-19 12:06:52 +0100

    upup version preview done
    Took 22 minutes

--- src/components/MetaVersion.tsx
+++ src/components/MetaVersion.tsx
@@ -1,11 +1,9 @@
-import * as packageJson from '../../package.json'
+import { LIB_VERSION } from '../version'
 
 const MetaVersion = () => {
     return (
         <>
-            <p className="text-xs text-[#9d9d9d] mb-4">
-                ًّUpup v{packageJson.version}
-            </p>
+            <p className="text-xs text-[#9d9d9d] mb-2">Upup v{LIB_VERSION}</p>
             <p className="text-xs text-[#9d9d9d] mb-4">Powered by uNotes</p>
         </>
     )
