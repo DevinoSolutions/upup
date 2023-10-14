d6435c925f7dedfc6a7acee6ea5e7e2eeb42526a
BASSEM <bassem.jadoui@esprit.tn>
2023-10-14 03:27:22 +0100

    tweak get client configs
    Took 34 minutes

--- src/lib/getClient.ts
+++ src/lib/getClient.ts
@@ -7,18 +7,24 @@ export type S3ClientWithSend = S3Client & {
 
 let client: S3ClientWithSend | null = null
 
+type Params = {
+    configs: S3Configs
+    handler?: any
+}
+
 /**
- * Get S3 client
- * @param configs S3 configs (region, endpoint...etc)
- * @returns S3 client
+ * Get S3 client singleton instance with custom request handler
+ * @param params
  */
-const getClient = (configs: S3Configs): S3ClientWithSend => {
+const getClient = (params: Params): S3ClientWithSend => {
     if (client) return client
+    const { configs, handler: requestHandler } = params
     const { region, endpoint, credentials } = configs
     client = new S3Client({
         region,
         endpoint,
         credentials,
+        requestHandler,
     }) as S3ClientWithSend
 
     return client
