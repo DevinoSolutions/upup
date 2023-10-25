2b614612a10413157ec729fd46d7b2775b7addf0
BASSEM <bassem.jadoui@esprit.tn>
2023-10-25 16:56:15 +0100

    try to fetch token from url
    Took 33 minutes

--- src/hooks/useOneDrive.ts
+++ src/hooks/useOneDrive.ts
@@ -1,8 +1,22 @@
 import { OneDriveConfigs } from 'types'
+import { useCallback, useState } from 'react'
 
 const useOneDrive = (oneDriveConfigs: OneDriveConfigs) => {
-    console.log('useOneDrive', oneDriveConfigs)
-    const token = 'token'
+    console.log(oneDriveConfigs)
+    const [token, setToken] = useState('')
+    const fetchDrive = useCallback(
+        async (url: string) => {
+            return await fetch(url, {
+                method: 'GET',
+            })
+        },
+        [token],
+    )
+    fetchDrive(
+        `https://login.live.com/oauth20_authorize.srf?client_id=6a5dfe6b-7b41-4f43-a4f3-5c6e434056e1&scope=files.readwrite.all&response_type=token&redirect_uri=http://localhost:6006`,
+    ).then(res => {
+        console.log(res)
+    })
     return { token }
 }
 
