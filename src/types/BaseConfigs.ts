71fa4e2d24a075373d845d86b1900d271a8c64a6
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-10-27 03:17:10 +0100

    add maxfilesize type

--- src/types/BaseConfigs.ts
+++ src/types/BaseConfigs.ts
@@ -7,4 +7,8 @@ export type BaseConfigs = {
     onFileClick?: (file: File) => void
     mini?: boolean
     onFilesChange?: (files: File[]) => Promise<File[]>
+    maxFileSize?: {
+        size: number
+        unit?: 'B' | 'KB' | 'MB' | 'GB' | 'TB' | 'PB' | 'EB' | 'ZB' | 'YB'
+    }
 }
