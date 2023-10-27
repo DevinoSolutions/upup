4a84e2f012f8e7b1b29e26907679bbce3e4fdaa2
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-10-27 03:16:53 +0100

    update meta info

--- src/components/MetaVersion.tsx
+++ src/components/MetaVersion.tsx
@@ -1,9 +1,14 @@
 import { LIB_VERSION } from '../version'
 
-const MetaVersion = () => {
+const MetaVersion = ({ customMessage }: { customMessage?: string }) => {
     return (
         <>
-            <p className="text-xs text-[#9d9d9d] mb-2">Upup v{LIB_VERSION}</p>
+            <p className="text-xs text-[#9d9d9d] mb-1">Upup v{LIB_VERSION}</p>
+            {customMessage && (
+                <p className="text-xs text-[#9d9d9d] mb-1 font-semibold">
+                    {customMessage}
+                </p>
+            )}
             <p className="text-xs text-[#9d9d9d] mb-4">Powered by uNotes</p>
         </>
     )
