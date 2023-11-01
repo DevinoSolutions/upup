7dac6e45ec07f6ec98541721cc67bfde7fe072f5
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-11-01 20:40:59 +0100

    add border to previews

--- src/components/UpupUploader/FileBrowser/LiOD.tsx
+++ src/components/UpupUploader/FileBrowser/LiOD.tsx
@@ -37,7 +37,7 @@ const ListItem = ({
         <img
             src={file.thumbnails.small.url}
             alt={file.name}
-            className="w-5 h-5 rounded-md"
+            className="w-5 h-5 rounded-md border"
             onError={handleImgError}
         />
     ) : (
