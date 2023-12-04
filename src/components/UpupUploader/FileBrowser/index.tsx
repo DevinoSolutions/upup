52ede3f5df71e1c5f0534e087ae204b6b9d438c4
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-12-04 23:27:24 +0100

    format code

--- src/components/UpupUploader/FileBrowser/index.tsx
+++ src/components/UpupUploader/FileBrowser/index.tsx
@@ -138,7 +138,7 @@ const FileBrowser = ({
                         })}
                     </ul>
                 ) : (
-                    <div className="flex flex-col items-center justify-center h-full">
+                    <div className="flex h-full flex-col items-center justify-center">
                         <h1 className="text-sm">No files found</h1>
                     </div>
                 )}
