637668ad211b87189ad3709797cfcea7fab1a473
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-03-01 14:25:14 +0100

    Update MetaVersion component

--- src/components/MetaVersion.tsx
+++ src/components/MetaVersion.tsx
@@ -16,13 +16,12 @@ const MetaVersion = ({
 
     return (
         <>
-            <p className="mb-1 text-xs text-[#9d9d9d]">Upup v{LIB_VERSION}</p>
             <p className="mb-1 text-xs font-semibold text-[#9d9d9d]">
                 {customMessage ? `${customMessage}, ` : ''}
                 {limit ? `Max ${limit} files, ` : ''}
                 {sizeLimit}
-            </p>
-            <p className="mb-4 text-xs text-[#9d9d9d]">Powered by uNotes</p>
+            </p>{' '}
+            <p className="mb-3 text-xs text-[#9d9d9d]">Upup v{LIB_VERSION}</p>
         </>
     )
 }
