244ffc416c0b4782a7e114c319b3c9fdb52b44e1
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-03-01 17:10:22 +0100

    Refactor theme container styles

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -3,7 +3,7 @@ import { create } from 'zustand'
 
 export const theme: Theme = {
     container:
-        '"relative flex h-[min(98svh,35rem)] w-full max-w-[min(98svh,46rem)] select-none flex-col overflow-hidden rounded-md border bg-[#f4f4f4] dark:bg-[#1f1f1f]"',
+        'container relative flex h-[min(98svh,35rem)] w-full max-w-[min(98svh,46rem)] select-none flex-col overflow-hidden rounded-md border bg-[#f4f4f4] dark:bg-[#1f1f1f]',
 }
 
 type ThemeStore = {
