3ad03cf2b2ddefc37732780568313c5439b0c67a
Iyadchafroud <chafroud.iyad@gmail.com>
2024-05-26 21:36:14 +0100

    refactor: refactoring `checkFileType` function
    add JSDoc .
    return `true` immediately if any file or criter to match  for improving performance.
    accepted file types to lowercase to be sure about case-insensitive
    Removed unnecessary regex and replaced it with simple method
    Encapsulated  logic in Array.some()

--- src/lib/checkFileType.ts
+++ src/lib/checkFileType.ts
@@ -1,20 +1,32 @@
-export function checkFileType(file: File, acceptedFiles: string) {
-    if (file && acceptedFiles && acceptedFiles !== '*') {
-        const acceptedFilesArray = Array.isArray(acceptedFiles)
-            ? acceptedFiles
-            : acceptedFiles.split(',')
-        const { name: fileName, type: mimeType } = file
-        const baseMimeType = mimeType.replace(/\/.*$/, '')
-        const extension = mimeType.split('/')[1]
 
-        return (
-            acceptedFilesArray.includes(extension) ||
-            acceptedFilesArray.includes(mimeType) ||
-            acceptedFilesArray.includes(`${baseMimeType}/*`) ||
-            acceptedFilesArray.some(
-                type => type.startsWith('.') && fileName.endsWith(type),
-            )
-        )
+/**
+ * Check if the file type matche the accepted file types.
+ * @param {File} file - The file to check.
+ * @param {string } acceptedFiles - Accepted file types. Can be file extensions ('jpg') or MIME types ('image/jpeg') 
+ * @returns {boolean} - True if the file type is accepted, false otherwise.
+ */
+export function checkFileType(file: File, acceptedFiles: string) {
+    if (!file || !acceptedFiles || acceptedFiles === '*') {
+        return true; 
     }
-    return true
+
+    const acceptedFilesArray = Array.isArray(acceptedFiles)
+        ? acceptedFiles
+        : acceptedFiles.split(',');
+
+    const { name: fileName, type: mimeType } = file;
+    const extension = fileName ? fileName.split('.').pop()?.toLowerCase() : '';
+
+    return acceptedFilesArray.some(type => {
+        type = type.trim().toLowerCase();
+        if (type.startsWith('.')) {
+            // Check by file extension
+            return type.slice(1) === extension;
+        } else {
+            // Check by MIME type
+            const baseMimeType = mimeType.split('/')[0];
+            return type === mimeType || type === `${baseMimeType}/*`;
+        }
+    });
 }
+
