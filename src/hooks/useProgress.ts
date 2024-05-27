dd105c9376f507af2ac495b74e3fc5fe317f13c5
Iyadchafroud <chafroud.iyad@gmail.com>
2024-05-27 17:41:15 +0100

    chore(prettier):  clean up  `ues progress` hook and `check file type ` function

--- src/hooks/useProgress.ts
+++ src/hooks/useProgress.ts
@@ -1,44 +1,50 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 
-import { XhrHttpHandler } from '@aws-sdk/xhr-http-handler';
+import { XhrHttpHandler } from '@aws-sdk/xhr-http-handler'
 
 // add interface
 interface ProgressHook {
-    progress: number;
-    handler: XhrHttpHandler;
+    progress: number
+    handler: XhrHttpHandler
 }
 
 const useProgress = (files: File[]): ProgressHook => {
     // handler can be a const because we do nt need to change it betwenn two render calls
-    const handler = new XhrHttpHandler({});
+    const handler = new XhrHttpHandler({})
 
-    const [progress, setProgress] = useState<number>(0);
+    const [progress, setProgress] = useState<number>(0)
     useEffect(() => {
         const handleUploadProgress = (xhr: ProgressEvent) => {
             if (xhr.lengthComputable) {
-                const newProgress = Math.round((xhr.loaded / xhr.total) * 100);
-                setProgress(newProgress);
+                const newProgress = Math.round((xhr.loaded / xhr.total) * 100)
+                setProgress(newProgress)
 
                 if (newProgress === 100) {
-                    handler.off(XhrHttpHandler.EVENTS.UPLOAD_PROGRESS, handleUploadProgress);
+                    handler.off(
+                        XhrHttpHandler.EVENTS.UPLOAD_PROGRESS,
+                        handleUploadProgress,
+                    )
                 }
             }
-        };
+        }
 
-        handler.on(XhrHttpHandler.EVENTS.UPLOAD_PROGRESS, handleUploadProgress);
+        handler.on(XhrHttpHandler.EVENTS.UPLOAD_PROGRESS, handleUploadProgress)
 
         // Cleanup event listener on unmount or when `files` change
         return () => {
-            handler.off(XhrHttpHandler.EVENTS.UPLOAD_PROGRESS, handleUploadProgress);
-            setProgress(0);
-        };
-    }, [files]);
+            handler.off(
+                XhrHttpHandler.EVENTS.UPLOAD_PROGRESS,
+                handleUploadProgress,
+            )
+            setProgress(0)
+        }
+    }, [files])
 
     return {
         progress,
         handler,
-    };
-};
+    }
+}
 
-export default useProgress;
+export default useProgress
--- src/lib/checkFileType.ts
+++ src/lib/checkFileType.ts
@@ -1,32 +1,30 @@
-
 /**
  * Check if the file type matche the accepted file types.
  * @param {File} file - The file to check.
- * @param {string } acceptedFiles - Accepted file types. Can be file extensions ('jpg') or MIME types ('image/jpeg') 
+ * @param {string } acceptedFiles - Accepted file types. Can be file extensions ('jpg') or MIME types ('image/jpeg')
  * @returns {boolean} - True if the file type is accepted, false otherwise.
  */
 export function checkFileType(file: File, acceptedFiles: string) {
     if (!file || !acceptedFiles || acceptedFiles === '*') {
-        return true; 
+        return true
     }
 
     const acceptedFilesArray = Array.isArray(acceptedFiles)
         ? acceptedFiles
-        : acceptedFiles.split(',');
+        : acceptedFiles.split(',')
 
-    const { name: fileName, type: mimeType } = file;
-    const extension = fileName ? fileName.split('.').pop()?.toLowerCase() : '';
+    const { name: fileName, type: mimeType } = file
+    const extension = fileName ? fileName.split('.').pop()?.toLowerCase() : ''
 
     return acceptedFilesArray.some(type => {
-        type = type.trim().toLowerCase();
+        type = type.trim().toLowerCase()
         if (type.startsWith('.')) {
             // Check by file extension
-            return type.slice(1) === extension;
+            return type.slice(1) === extension
         } else {
             // Check by MIME type
-            const baseMimeType = mimeType.split('/')[0];
-            return type === mimeType || type === `${baseMimeType}/*`;
+            const baseMimeType = mimeType.split('/')[0]
+            return type === mimeType || type === `${baseMimeType}/*`
         }
-    });
+    })
 }
-
