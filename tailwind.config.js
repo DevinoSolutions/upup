c414352241d158b6c0ef20bfe78a9e1d9a155d55
MedAmine1212 <khaili.amine@hotmail.fr>
2025-05-19 18:39:21 +0100

    add prefix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     prefix: 'upup-',
+    important: '.upup-scope',
     content: ['./src/**/*.{tsx,ts,css}'],
     darkMode: 'class',
     theme: {
