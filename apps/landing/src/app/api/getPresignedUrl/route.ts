4dc812d06bef2c8faa697dac1ad47e136df1222e
kd_kinuthiadavid <kd.kinuthiadavid@gmail.com>
2026-02-10 20:42:29 +0300

    fix: add try-catch block to getPresignedUrl route
    allows us to catch errors and return them to the frontend in a format that the frontend is already expecting

--- apps/landing/src/app/api/getPresignedUrl/route.ts
+++ apps/landing/src/app/api/getPresignedUrl/route.ts
@@ -2,23 +2,30 @@ import { NextRequest, NextResponse } from "next/server";
 import { s3GeneratePresignedUrl } from "upup-react-file-uploader/server";
 
 export async function POST(req: NextRequest) {
-    const body = await req.json();
-    const { provider, enableAutoCorsConfig, ...fileParams } = body;
-    const presigned = await s3GeneratePresignedUrl({
-        origin: req.headers.get("origin") ?? "",
-        provider,
-        fileParams,
-        bucketName: process.env.S3_BUCKET!,
-        enableAutoCorsConfig,
-        s3ClientConfig: {
-            region: process.env.S3_REGION!,
-            endpoint: process.env.S3_ENDPOINT!,
-            credentials: {
-                accessKeyId: process.env.S3_KEY_ID!,
-                secretAccessKey: process.env.S3_SECRET!,
+    try {
+        const body = await req.json();
+        const { provider, enableAutoCorsConfig, ...fileParams } = body;
+        const presigned = await s3GeneratePresignedUrl({
+            origin: req.headers.get("origin") ?? "",
+            provider,
+            fileParams,
+            bucketName: process.env.S3_BUCKET!,
+            enableAutoCorsConfig,
+            s3ClientConfig: {
+                region: process.env.S3_REGION!,
+                endpoint: process.env.S3_ENDPOINT!,
+                credentials: {
+                    accessKeyId: process.env.S3_KEY_ID!,
+                    secretAccessKey: process.env.S3_SECRET!,
+                },
             },
-        },
-    });
+        });
 
-    return NextResponse.json(presigned, { status: 200 });
+        return NextResponse.json(presigned, { status: 200 });
+    } catch (error) {
+        return NextResponse.json(
+            { details: (error as Error).message },
+            { status: 500 },
+        );
+    }
 }
