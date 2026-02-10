9daa0d4bbfc6c6bda7dc12317d833cf6d29570db
kd_kinuthiadavid <kd.kinuthiadavid@gmail.com>
2026-02-10 23:02:02 +0300

    build: add locales sub-path export and enable code splitting

--- packages/upup/src/index.browser.ts
+++ packages/upup/src/index.browser.ts
@@ -3,11 +3,13 @@ export {
     default as UpupUploader,
     UpupUploaderRef,
 } from './frontend/UpupUploader'
+export { en_US } from './shared/i18n'
 export {
     FileWithParams,
     FileWithProgress,
     GoogleDriveConfigs,
     OneDriveConfigs,
+    Translations,
     UploadAdapter,
     UpupProvider,
 } from './shared/types'
